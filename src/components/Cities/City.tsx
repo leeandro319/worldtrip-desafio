57d7cc086d28d146daa808a1f2155f9e9cdad317
leandro guaraldi <leandro_319@yahoo.com.br>
2021-11-14 03:17:55 -0300

    Desafio OK

---
+++ src/components/Cities/City.tsx
@@ -0,0 +1,16 @@
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+
+export default function City() {
+ return (
+  <Box borderRadius="4px" overflow="hidden">
+    <Image src={'/londres.jpg'} alt='Londres' h="170px" w="100%"/>
+    <Flex p="6" align="center" justify="space-between" bg="white" border="1px" borderColor="yellow.300" borderTop="0">
+      <Flex direction="column">
+        <Heading fontSize="xl" fontWeight="500">Londres</Heading>
+        <Text mt="3" fontSize="md" color="gray.500" fontWeight="500">França</Text>
+      </Flex>
+      <Image src={'/cidade.jpg'} w="30px" h="30px" borderRadius="50%" objectFit="cover"/>
+    </Flex>
+  </Box>
+ )
+}
